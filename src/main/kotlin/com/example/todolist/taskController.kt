74a5a9bebaf99b8297b4b28de387712696af38ca
sinagaki58 <s.inagaki58@gmail.com>
2019-07-15 21:40:14 +0900

    initial commit

---
+++ src/main/kotlin/com/example/todolist/taskController.kt
@@ -0,0 +1,17 @@
+package com.example.todolist
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("tasks")
+class TaskController(private val taskRepository: TaskRepository) {
+    @GetMapping("")
+    fun index(model: Model):String {
+        val tasks = taskRepository.findAll()
+        model.addAttribute("tasks", tasks)
+        return "tasks/index"
+    }
+}

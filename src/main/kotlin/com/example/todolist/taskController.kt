ad7681f5f6317dfea0b848233766a756ecbc19d8
sinagaki58 <s.inagaki58@gmail.com>
2019-07-15 22:48:49 +0900

    add actions

--- src/main/kotlin/com/example/todolist/taskController.kt
+++ src/main/kotlin/com/example/todolist/taskController.kt
@@ -2,16 +2,57 @@ package com.example.todolist
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
 
 @Controller
 @RequestMapping("tasks")
 class TaskController(private val taskRepository: TaskRepository) {
     @GetMapping("")
-    fun index(model: Model):String {
+    fun index(model: Model): String {
         val tasks = taskRepository.findAll()
         model.addAttribute("tasks", tasks)
         return "tasks/index"
+    }
+
+    @GetMapping("new")
+    fun new(form: TaskCreateForm): String {
+        return "tasks/new"
+    }
+
+    @GetMapping("{id}/edit")
+    fun edit(@PathVariable("id") id: Long,
+             form: TaskUpdateForm):String {
+        val task = taskRepository.findById(id) ?: throw NotFoundException()
+        form.content = task.content
+        form.done = task.done
+        return "tasks/edit"
+    }
+
+
+    @PostMapping("")
+    fun create(@Validated form: TaskCreateForm,
+               bindResult: BindingResult): String {
+        if (bindResult.hasErrors())
+            return "tasks/new"
+
+        val content = requireNotNull(form.content)
+        taskRepository.create(content)
+        return "redirect:/tasks"
+    }
+
+    @PatchMapping("{id")
+    fun update(@PathVariable("id")id:Long,
+               @Validated form: TaskUpdateForm,
+               bindResult: BindingResult):String {
+        if (bindResult.hasErrors())
+            return "tasks/edit"
+
+        val task = taskRepository.findById(id) ?: throw NotFoundException()
+        val newTask = task.copy(content = requireNotNull(form.content),
+                done = form.done)
+        taskRepository.update(newTask)
+        return "redirect:/tasks"
     }
 }

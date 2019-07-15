68136bcd91a8c9882678207937a7fe47fad4990f
sinagaki58 <s.inagaki58@gmail.com>
2019-07-16 07:00:13 +0900

    add exception page

--- src/main/kotlin/com/example/todolist/taskController.kt
+++ src/main/kotlin/com/example/todolist/taskController.kt
@@ -1,5 +1,6 @@
 package com.example.todolist
 
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
@@ -55,4 +56,8 @@ class TaskController(private val taskRepository: TaskRepository) {
         taskRepository.update(newTask)
         return "redirect:/tasks"
     }
+
+    @ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNotFoundException():String = "tasks/not_found"
 }
